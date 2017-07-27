32b6a7de40c2e2ba6a940bcce3624a53c10beedf
Cu Duy <cuduy197@gmail.com>
2017-07-28 06:26:28 +0700

    Update test.sh

--- test.sh
+++ test.sh
@@ -4,3 +4,23 @@ yellow="\033[33;1;7m"
 end='\033[0m'
 
 printf "${green} Test OK!  \n${end}"
+
+show_spinner()
+{
+  local -r pid="${1}"
+  local -r delay='0.75'
+  local spinstr='\|/-'
+  local temp
+  while ps a | awk '{print $1}' | grep -q "${pid}"; do
+    temp="${spinstr#?}"
+    printf " [%c]  " "${spinstr}"
+    spinstr=${temp}${spinstr%"${temp}"}
+    sleep "${delay}"
+    printf "\b\b\b\b\b\b"
+  done
+  printf "    \b\b\b\b"
+}
+
+("$@") &
+show_spinner "$!"
+spinner sleep 10
